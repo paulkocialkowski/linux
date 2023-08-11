d6dfb5d2b28768b39b8200d30080ec9c285420aa
Ruan Jinjie <ruanjinjie@huawei.com>
2023-08-11 10:49:45 +0800

    media: staging: media: sunxi: cedrus: Remove redundant of_match_ptr()
    The driver depends on CONFIG_OF, it is not necessary to use
    of_match_ptr() here.
    
    Signed-off-by: Ruan Jinjie <ruanjinjie@huawei.com>
    Acked-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Acked-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- drivers/staging/media/sunxi/cedrus/cedrus.c
+++ drivers/staging/media/sunxi/cedrus/cedrus.c
@@ -695,7 +695,7 @@ static struct platform_driver cedrus_driver = {
 	.remove		= cedrus_remove,
 	.driver		= {
 		.name		= CEDRUS_NAME,
-		.of_match_table	= of_match_ptr(cedrus_dt_match),
+		.of_match_table	= cedrus_dt_match,
 		.pm		= &cedrus_dev_pm_ops,
 	},
 };
